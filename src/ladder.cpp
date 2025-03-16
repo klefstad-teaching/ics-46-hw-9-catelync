fa396cc257de6377a745edb3403675bb8dc05d21
Catelynn Chen <catelync@uci.edu>
2025-03-16 05:14:52 +0000

    typo worte count

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -105,7 +105,7 @@ void print_word_ladder(const vector<string>& ladder)
     }
     for (size_t i = 0; i < ladder.size(); ++i)
     {
-        count << ladder[i];
+        cout << ladder[i];
         if (i < ladder.size() - 1)
         {
             cout << ladder[i];
