1fe99e66215a8fd8b17a11cabbbca4b87db575f9
Catelynn Chen <catelync@uci.edu>
2025-03-16 03:07:31 +0000

    did dij shortest paht

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -7,6 +7,34 @@ vector<int> dijkstras_shortest_path(const Graph& G, int source, vector<int>& pre
     vector<int> distances(n, INF);
     vector<bool> visited(n, false);
     previous.assign(n, -1);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
+    distances[source] = 0;
+    minHeap.push({0, source});
+
+    while (!minHeap.empty())
+    {
+        int u = minHeap.top().second;
+        minHeap.pop();
+
+        if (visited[u])
+        {
+            continue;
+        }
+        visited[u] = true;
+
+        for (const Edge& edge : G[u])
+        {
+            int v = edge.dst;
+            int weight = edge.weight;
+            if (!visited[v] && distances[u] + weight < distances[v])
+            {
+            distances[v] = distances[u] + weight;
+            previous[v] = u;
+            minHeap.push({distances[v], v});
+            }
+        }
+    }
+    return distances;
 }
 
 // get the shortest path from the source to given dest
