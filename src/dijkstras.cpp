4ebd2c1690397b2c7cdb2285bc54a554e8f07e4f
Catelynn Chen <catelync@uci.edu>
2025-03-16 02:48:45 +0000

    initalized needed vars

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -1,6 +1,13 @@
 # include "dijkstras.h"
 
 // implement dij given source vertex 
+vector<int> dijkstras_shortest_path(const Graph& G, int source, vector<int>& previous)
+{
+    int n = G.numVerticies;
+    vector<int> distances(n, INF);
+    vector<bool> visited(n, false);
+    previous.assign(n, -1);
+}
 
 // get the shortest path from the source to given dest
 
