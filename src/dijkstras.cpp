a74db3b5bb8aae41d1995a52f7245efc904a7573
Catelynn Chen <catelync@uci.edu>
2025-03-17 00:10:36 +0000

    fixed the print path

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -61,13 +61,9 @@ void print_path(const vector<int>& path, int total_cost)
         cout << "No path found" << endl;
         return;
     }
-    for (size_t i = 0; i <path.size(); ++i)
+    for (size_t i = 0; i < path.size(); ++i)
     {
-        cout << path[i];
-        if (i < path.size() - 1) 
-        {
-            cout << " ";
-        }
+        cout << path[i] << " ";
     }
     cout << "\nTotal cost is " << total_cost << endl; 
 }
